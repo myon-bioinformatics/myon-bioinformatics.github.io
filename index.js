d0f599d1e358298d314c8dc08dc166868b2675a2
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2023-06-19 19:46:15 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 // index.js
 
-window.addEventListener('DOMContentLoaded', function() {
+// Wait for the DOM content to load
+window.addEventListener('DOMContentLoaded', function () {
+    // Select all h3 and h4 elements
     var elements = document.querySelectorAll('h3, h4');
 
-    elements.forEach(function(element) {
+    // Iterate through each element
+    elements.forEach(function (element) {
+        // Get the innerHTML of the element
         var text = element.innerHTML;
+
+        // Replace '&lt;' and '&gt;' with highlighted spans
         var highlighted = text.replace(/&lt;([^&]+)&gt;/g, '<span class="highlight">&lt;$1&gt;</span>');
+
+        // Update the innerHTML with the highlighted text
         element.innerHTML = highlighted;
     });
 });
